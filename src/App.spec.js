44dc15862557cc42c5f4096633d419499f43c524
Jean Silva <jeancarlos.silva@me.com>
2017-11-27 09:20:22 -0200

    initial commit

---
+++ src/App.spec.js
@@ -0,0 +1,8 @@
+/* global it, expect */
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+it('renders without crashing', () => {
+  expect(shallow(<App />).exists()).toBeTruthy();
+});
