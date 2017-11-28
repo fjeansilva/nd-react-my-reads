538a64736b44eec5a4d99f3f2b2bb23e9a13e5ee
Jean Silva <jeancarlos.silva@me.com>
2017-11-28 11:26:29 -0200

    refactor: Refactoring the Bookshelf component
    Included PropTypes to validate the props of the component.
    Adding html structure required and a call of map function to
    list the books.

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Book from './Book';
 
-const Bookshelf = () => (
-  <h1>Bookshelf</h1>
+const Bookshelf = ({ title, books }) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{title}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {books.map(b => (
+          <li key={b.title}><Book title={b.title} authors={b.authors} cover={b.cover} /></li>
+        ))}
+      </ol>
+    </div>
+  </div>
 );
+
+Bookshelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+};
 
 export default Bookshelf;
