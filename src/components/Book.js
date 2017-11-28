f7dac28c0833db49a1bca507781388f6ef45368f
Jean Silva <jeancarlos.silva@me.com>
2017-11-28 09:52:00 -0200

    Refactor: Implemented the Book component
    Refactoring the Book component coverging all rules of the test.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
 
-const Book = () => (
-  <h1>Book</h1>
+const Book = ({ title, authors, cover }) => (
+  <div className="book">
+    <div className="book-top">
+      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${cover}"` }} />
+      <div className="book-shelf-changer">
+        <select>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    </div>
+    <div className="book-title">
+      {title}
+    </div>
+    <div className="book-authors">
+      {authors}
+    </div>
+  </div>
 );
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  authors: PropTypes.string.isRequired,
+  cover: PropTypes.string.isRequired,
+};
 
 export default Book;
