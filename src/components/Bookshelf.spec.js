af771151966bedf907c903e3fe6155d82f8d15ab
Jean Silva <jeancarlos.silva@me.com>
2017-11-28 11:20:32 -0200

    test: Refactoring tests of the Bookshelf component
    Included the following rules:
      - The Bookshelf component render with success.
      - The Bookshelf component should have a title.
      - The Bookshelf component should have a list of books.

--- src/components/Bookshelf.spec.js
+++ src/components/Bookshelf.spec.js
@@ -1,10 +1,49 @@
 /* global describe, it, expect */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Bookshelf from './Bookshelf';
 
 describe('Bookshelf component', () => {
+  const books = [
+    {
+      title: 'To Kill a Mockingbird',
+      authors: 'Harper Lee',
+      cover: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
+    },
+    {
+      title: 'The Hobbit',
+      authors: 'J.R.R. Tolkien',
+      cover: 'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api',
+    },
+  ];
+  const element = <Bookshelf title="Reading" books={books} />;
+  const wrapper = mount(element);
+  const grid = wrapper.find('.bookshelf-books > ol.books-grid');
+
   it('should render successfully', () => {
-    expect(shallow(<Bookshelf />));
+    const component = shallow(element);
+    expect(component.find('.bookshelf').exists()).toBeTruthy();
+  });
+
+  it('should have a prop title', () => {
+    expect(wrapper.prop('title')).toEqual('Reading');
+  });
+
+  it('should have a prop books', () => {
+    expect(wrapper.prop('books').length).toBe(2);
+  });
+
+  it('should render a title', () => {
+    const title = wrapper.find('h2.bookshelf-title');
+    expect(title.exists()).toBeTruthy();
+    expect(title.text()).toEqual('Reading');
+  });
+
+  it('should render a grid', () => {
+    expect(grid.exists()).toBeTruthy();
+  });
+
+  it('should render a grid with two books', () => {
+    expect(grid.find('li .book .book-title').length).toBe(2);
   });
 });
