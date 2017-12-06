404fa7f1bee9e6a57269915c468584df39722906
Jean Silva <jeancarlos.silva@me.com>
2017-12-06 10:44:13 -0200

    refactor: Refactoring BookApp component Included BookShelf component, book state and componentDidMount method.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import BookShelf from './Bookshelf';
+import { getAll } from '../utils/BooksAPI';
 
-const BooksApp = () => (
-  <div className="app">
-    <div className="list-books">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
+class BooksApp extends Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    getAll().then((books) => this.setState({ books }));
+  }
+
+  render() {
+    const { books } = this.state;
+
+    return (
+      <div className="app">
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <BookShelf title="Currently Reading" books={books} />
+            <BookShelf title="Want to Read" books={books} />
+            <BookShelf title="Read" books={books} />
+          </div>
+        </div>
       </div>
-      <div className="list-books-content">
-        <div className="bookshelf" />
-        <div className="bookshelf" />
-        <div className="bookshelf" />
-      </div>
-    </div>
-  </div>
-);
+    )
+  }
+}
 
 export default BooksApp;
