3218bd3c81498b86f63371010e1e5e38510491d9
Jean Silva <jeancarlos.silva@me.com>
2018-01-04 11:42:34 -0200

    test: Refactoring test of App component Included the following tests:   - Should call filterByShelf   - Should change books between shelves

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,16 +1,42 @@
-/* global it, expect, describe, jest, beforeEach */
+/* global it, fit, expect, describe, jest, beforeEach */
 import React from 'react';
 import { spy } from 'sinon';
 import { shallow, mount } from 'enzyme';
 import BookApp from './App';
+import Bookshelf from './Bookshelf';
 
 jest.mock('../utils/BooksAPI.js');
+const filterByShelf = jest.fn();
+const filterEach = (fn, arr) => {
+  arr.map(a => fn(a));
+};
 
 describe('BookApp component', () => {
   let wrapper;
+  let books;
 
   beforeEach(() => {
     wrapper = mount(<BookApp />);
+    books = [
+      {
+        id: 'abc',
+        title: 'To Kill a Mockingbird',
+        authors: ['Harper Lee'],
+        imageLinks: {
+          thumbnail: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
+        },
+        shelf: 'read',
+      },
+      {
+        id: 'def',
+        title: 'The Hobbit',
+        authors: ['J.R.R. Tolkien'],
+        imageLinks: {
+          thumbnail: 'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api'
+        },
+        shelf: 'read',
+      },
+    ];
   });
 
   it('should render successfully', () => {
@@ -39,22 +65,6 @@ describe('BookApp component', () => {
   });
 
   it('should have two books', () => {
-    const books = [
-      {
-        title: 'To Kill a Mockingbird',
-        authors: 'Harper Lee',
-        imageLinks: {
-          thumbnail: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
-        },
-      },
-      {
-        title: 'The Hobbit',
-        authors: 'J.R.R. Tolkien',
-        imageLinks: {
-          thumbnail: 'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api'
-        },
-      },
-    ];
     wrapper.setState({ books: [] });
     expect(wrapper.state('books').length).toBe(0);
     wrapper.setState({ books });
@@ -66,6 +76,18 @@ describe('BookApp component', () => {
     mount(<BookApp />);
     expect(BookApp.prototype.componentDidMount.calledOnce).toBeTruthy();
     componentDidMountSpy.restore();
+  });
+
+  it('should call filterByShelf three times', () => {
+    filterEach(filterByShelf, ['currentlyReading', 'wantToRead', 'read']);
+    expect(filterByShelf).toHaveBeenCalledTimes(3);
+  });
+
+  fit('should change book of shelf Want to Read to Currently Reading', () => {
+    wrapper.setState({ books });
+    expect(wrapper.find(Bookshelf).first().props().books.length).toBe(0);
+    wrapper.find('.book-top .book-shelf-changer > select').first().simulate('change', { target: { value: 'currentlyReading' } });
+    expect(wrapper.find(Bookshelf).first().props().books.length).toBe(1);
   });
 });
 
