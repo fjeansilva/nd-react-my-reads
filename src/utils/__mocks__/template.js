6b6cd4a74e0586ed2aa117e6e77a3335fc38de4c
Jean Silva <jeancarlos.silva@me.com>
2018-01-16 09:47:23 -0200

    test: Template with default html result This file contain a result default to test the return html.

---
+++ src/utils/__mocks__/template.js
@@ -0,0 +1,17 @@
+export default '<div class="search-books-results"><ol class="books-grid">' +
+'<li><div class="book"><div class="book-top"><div class="book-cover" style="width: 128px; height: 193px;">' +
+'</div><div class="book-shelf-changer"><select><option value="" disabled="">Move to...</option>' +
+'<option value="currentlyReading">Currently Reading</option><option value="wantToRead" selected="">Want to Read</option>' +
+'<option value="read">Read</option><option value="none">None</option></select></div></div>' +
+'<div class="book-title">React</div><div class="book-authors">Slash Axl Rose</div></div>' +
+'</li><li><div class="book"><div class="book-top"><div class="book-cover" style="width: 128px; height: 193px;">' +
+'</div><div class="book-shelf-changer"><select><option value="" disabled="">Move to...</option>' +
+'<option value="currentlyReading">Currently Reading</option><option value="wantToRead" selected="">Want to Read</option>' +
+'<option value="read">Read</option><option value="none">None</option></select></div></div>' +
+'<div class="book-title">React</div><div class="book-authors">Bruce Dickinson Dave Murray</div>' +
+'</div></li><li><div class="book"><div class="book-top"><div class="book-cover" style="width: 128px; height: 193px;">' +
+'</div><div class="book-shelf-changer"><select><option value="" disabled="">Move to...</option>' +
+'<option value="currentlyReading">Currently Reading</option><option value="wantToRead" selected="">Want to Read</option>' +
+'<option value="read">Read</option><option value="none">None</option></select></div></div>' +
+'<div class="book-title">React</div><div class="book-authors">Angus Young Bono Scoth</div>' +
+'</div></li></ol></div>';
