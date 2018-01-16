613e5b01374bc41481a0c7be998827fdd07ddb7c
Jean Silva <jeancarlos.silva@me.com>
2018-01-16 09:21:09 -0200

    test: Input test Included the following rules:   - Should render successfully.   - Should render an input.   - Should contain an onChange prop.   - Should contain a placeholder prop.

---
+++ src/components/Input.spec.js
@@ -0,0 +1,26 @@
+/* global describe, it, expect */
+import React from 'react';
+import { shallow } from 'enzyme';
+import { spy } from 'sinon';
+import Input from './Input';
+
+describe('Input component', () => {
+  const onChange = spy();
+  const wrapper = shallow(<Input onChange={onChange} placeholder="Search by title or author" />);
+
+  it('Should render successfully', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('Should render a input', () => {
+    expect(wrapper.find('input').exists()).toBeTruthy();
+  });
+
+  it('Should contain a onChange prop', () => {
+    expect(wrapper.prop('onChange')).toEqual(onChange);
+  });
+
+  it('Should contain a placeholder prop', () => {
+    expect(wrapper.prop('placeholder')).toEqual('Search by title or author');
+  });
+});
