341563146befc509822da3699178c0df7779fd9a
Jean Silva <jeancarlos.silva@me.com>
2018-01-17 11:15:57 -0200

    test: Add MemoryRouter Included MemoryRouter to wrapper the Search component, it's necessary to keep the history of url in memory.

--- src/components/Search.spec.js
+++ src/components/Search.spec.js
@@ -1,5 +1,6 @@
 /* global describe, it, expect, beforeEach, jest */
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { shallow, mount } from 'enzyme';
 import Search from './Search';
 import Input from './Input';
@@ -8,55 +9,59 @@ import SearchBooks from './SearchBooks';
 jest.mock('../utils/BooksAPI.js');
 
 describe('Search Component', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = mount(<Search />);
-  });
-
   it('Should render successfully', () => {
-    expect(shallow(<Search />).exists()).toBeTruthy();
+    expect(shallow(<MemoryRouter initialEntries={['/', '/search']} initialIndex={1}><Search /></MemoryRouter>).exists()).toBeTruthy();
   });
 
   it('Should render wrapper', () => {
+    const wrapper = mount(<MemoryRouter initialEntries={['/', '/search']} initialIndex={1}><Search /></MemoryRouter>);
     expect(wrapper.find('.search-books').exists()).toBeTruthy();
   });
 
   it('Should contain a search bar', () => {
+    const wrapper = mount(<MemoryRouter initialEntries={['/', '/search']} initialIndex={1}><Search /></MemoryRouter>);
     expect(wrapper.find('.search-books .search-books-bar').exists()).toBeTruthy();
   });
 
   it('Should contain a link to home page', () => {
+    const wrapper = mount(<MemoryRouter initialEntries={['/', '/search']} initialIndex={1}><Search /></MemoryRouter>);
     expect(wrapper.find('.search-books-bar a.close-search').exists()).toBeTruthy();
   });
 
   it('Should contain a wrapper to the input', () => {
+    const wrapper = mount(<MemoryRouter initialEntries={['/', '/search']} initialIndex={1}><Search /></MemoryRouter>);
     expect(wrapper.find('.search-books-bar .search-books-input-wrapper').exists()).toBeTruthy();
   });
 
   it('Should render an input', () => {
+    const wrapper = mount(<MemoryRouter initialEntries={['/', '/search']} initialIndex={1}><Search /></MemoryRouter>);
     expect(wrapper.find(Input).exists()).toBeTruthy();
   });
 
   it('Should pass a value to query state', () => {
+    const wrapper = mount(<MemoryRouter initialEntries={['/', '/search']} initialIndex={1}><Search /></MemoryRouter>);
     wrapper.find(Input).simulate('change', { target: { value: 'Android' } });
-    expect(wrapper.state('query')).toEqual('Android');
+    expect(wrapper.find(Search).instance().state).toEqual({ query: 'Android' });
   });
 
   it('Must have an empty value in query state', () => {
+    const wrapper = mount(<MemoryRouter initialEntries={['/']} initialIndex={0}><Search /></MemoryRouter>);
     wrapper.find(Input).simulate('change', { target: { value: 'xx' } });
-    expect(wrapper.state('query')).toEqual('');
+    expect(wrapper.find(Search).instance().state).toEqual({ query: '' });
     wrapper.find(Input).simulate('change', { target: { value: 'x' } });
-    expect(wrapper.state('query')).toEqual('');
+    expect(wrapper.find(Search).instance().state).toEqual({ query: '' });
     wrapper.find(Input).simulate('change', { target: { value: '' } });
-    expect(wrapper.state('query')).toEqual('');
+    expect(wrapper.find(Search).instance().state).toEqual({ query: '' });
   });
 
   it('Must have a value in query state', () => {
+    const wrapper = mount(<MemoryRouter initialEntries={['/']} initialIndex={0}><Search /></MemoryRouter>);
     wrapper.find(Input).simulate('change', { target: { value: 'xxx' } });
-    expect(wrapper.state('query')).toEqual('xxx');
+    expect(wrapper.find(Search).instance().state).toEqual({ query: 'xxx' });
   });
 
   it('Should render a SearchBooks component', () => {
+    const wrapper = mount(<MemoryRouter initialEntries={['/']} initialIndex={0}><Search /></MemoryRouter>);
     expect(wrapper.find(SearchBooks).exists()).toBeTruthy();
   });
 });
