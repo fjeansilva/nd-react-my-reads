899aae3b1f9db14fea1a82b8190ba1df3bfbc4ae
Jean Silva <jeancarlos.silva@me.com>
2018-01-17 14:48:31 -0200

    refactor: Added Spinner and EmptyShelf Added Spinner and EmptyShelf component to show loader or empty shelf to the user.

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Spinner from 'react-spinkit';
 import Book from './Book';
+import EmptyShelf from './EmptyShelf';
 
-const Bookshelf = ({ title, books, onUpdateBook }) => (
+
+const Bookshelf = ({
+  title, books, onUpdateBook, loading,
+}) => (
   <div className="bookshelf">
     <h2 className="bookshelf-title">{title}</h2>
     <div className="bookshelf-books">
       <ol className="books-grid">
+        {loading && (
+          <Spinner name="ball-scale-ripple" color="green" />
+        )}
+        {books.length === 0 && !loading && (
+          <EmptyShelf />
+        )}
         {books.map(b => (
           <li key={b.id}>
             <Book
@@ -24,6 +35,7 @@ Bookshelf.propTypes = {
   title: PropTypes.string.isRequired,
   books: PropTypes.array.isRequired,
   onUpdateBook: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default Bookshelf;
