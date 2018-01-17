13dc98a1cb7ffe268b749af292c6b5e68617a7e3
Jean Silva <jeancarlos.silva@me.com>
2018-01-17 11:13:17 -0200

    refactor: Add BrowserRouter Included BrowserRouter of React Router to manager the navigation.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 /* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter forceRefresh><App /></BrowserRouter>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
